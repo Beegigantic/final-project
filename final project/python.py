e50fec5d702ee688b3770ae0bdbe9d5a97148bb9
Beegigantic <melanie.c.janzen@gmail.com>
2025-01-07 10:35:36 -0800

    Update python.py

--- final project/python.py
+++ final project/python.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
-import plotly.graph_objs as go  # Ensure this import is correct
+import plotly.graph_objs as go
 import plotly.offline as pyo
 import os
 
@@ -15,7 +15,7 @@ def create_graph():
     )]
     layout = go.Layout(
         title='Sample Bar Chart',
-        height=500  # Set a specific height for the chart
+        height=500
     )
     fig = go.Figure(data=data, layout=layout)
     graph = pyo.plot(fig, output_type='div')
@@ -28,49 +28,16 @@ def index():
         name = request.form['username']  # Capture username from the form
         return render_template('index.html', graph=graph, name=name)
     
-    # If no username, just render the page with the graph
     return render_template('index.html', graph=graph)
-
-@app.route('/html.html')
-def html_page():
-    return render_template('html.html')
-
-@app.route('/python.html')
-def python_page():
-    return render_template('python.html')
-
-@app.route('/cSharp.html')
-def cSharp_page():
-    return render_template('cSharp.html')
-
-@app.route('/cPlusPlus.html')
-def cPlusPlus_page():
-    return render_template('cPlusPlus.html')
-
-@app.route('/javascript.html')
-def javascript_page():
-    return render_template('javascript.html')
-messages = []
-
 
 @socketio.on('connect')
 def handle_connect():
-    # Send existing messages to the newly connected client
-    for msg in messages:
-        send(msg)
-        
+    pass
+
 @socketio.on('message')
 def handle_message(msg):
-    print(f"Message: {msg}")
-    # Add the new message to the global list
-    messages.append(msg)
-    # Keep only the last 10 messages
-    if len(messages) > 10:
-        messages.pop(0)
-    # Broadcast the message to all clients
     send(msg, broadcast=True)
 
-    
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Ensure the PORT environment variable is used
+    port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
